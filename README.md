39d03fcf3a11fec7b3c3c0371181ce52730d947c
Lourens Rozema <lrozema@embed.nl>
2015-04-18 22:10:39 +0200

    Initial commit of a CP.

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 The DNS is obtained from https://github.com/nodemcu/nodemcu-firmware/issues/59 and improved. It allows all requests to direct to 192.168.4.1.
 
+## Captive portal
+
+All the code together forms a captive portal that loads a page to control an RGB LED.
+
--- dns.lua
+++ dns.lua
@@ -1,6 +1,3 @@
--- This code is probably not MIT, for copyright look somewhere here:
---   https://svn.nmap.org/!svn/bc/31535/nmap-exp/d33tah/lua-exec-examples/ncat/scripts/dns.lua
---   https://github.com/nodemcu/nodemcu-firmware/issues/59
 
 s=net.createServer(net.UDP)
 s:on("receive",function(s,c) 
@@ -27,7 +24,7 @@ s:on("receive",function(s,c)
         query = query .. '.'
     end
     query=query:sub(1,query:len()-1) --strip the trailing dot.
-    print(query)
+    --print(query)
     q_type = string.sub(c,j,j+1)
     j=j+2
     if q_type == string.char(0)..string.char(1) then
